c389d0dad28bed3442f085c866e610c1d045b6c4
InkedCat <146424693+InkedCat@users.noreply.github.com>
2024-03-19 23:31:03 +0100

    feat: clean ChatTestApplication class

--- lib/ChatTestApplication.ts
+++ lib/ChatTestApplication.ts
@@ -1,33 +1,8 @@
 import { Backend } from "kuzzle";
-import { PrometheusPlugin } from "kuzzle-plugin-prometheus";
-
-export type ChatTestApplicationConfig = {
-  someValue: string;
-
-  another: {
-    value: number;
-  };
-};
 
 export class ChatTestApplication extends Backend {
-  public configuration: ChatTestApplicationConfig;
-  private prometheusPlugin = new PrometheusPlugin();
-
-  get appConfig() {
-    return this.config.content.application as ChatTestApplicationConfig;
-  }
-
-  constructor(config?: ChatTestApplicationConfig) {
+  constructor() {
     super("chat-test-application");
-
-    if (config) {
-      this.configuration = config;
-    } else {
-      this.configuration = this.config.content
-        .application as ChatTestApplicationConfig;
-    }
-
-    this.plugin.use(this.prometheusPlugin);
   }
 
   async start() {
