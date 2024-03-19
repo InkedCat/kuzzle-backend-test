2b59aa620035eaf929a71d3c4c0eecc4385b0d09
InkedCat <146424693+InkedCat@users.noreply.github.com>
2024-03-20 00:04:08 +0100

    feat: add getMessages endpoint

--- lib/modules/ChatController.ts
+++ lib/modules/ChatController.ts
@@ -1,15 +1,36 @@
-import { Backend, Controller } from "kuzzle";
+import { Backend, BadRequestError, Controller, KuzzleRequest } from "kuzzle";
+import { ChatService } from "./ChatService";
 
 class ChatController extends Controller {
+    private service: ChatService;
 
-    constructor(app: Backend) {
+    constructor(app: Backend, service: ChatService) {
         super(app);
+        this.service = service;
 
         this.definition = {
             actions : {
-
+                getMessages: {
+                    handler: this.getMessages,
+                    http: [
+                        { verb: "get", path: "chat/messages" }
+                    ]
+                }
             }
         };
+    }
+
+    async getMessages(request: KuzzleRequest) {
+        if(request.input.args.from < 0 || request.input.args.size < 0) {
+            throw new BadRequestError("from and size must be positive numbers");
+        }
+
+        const from = request.input.args.from || 0;
+        const size = request.input.args.size || 100;
+
+        console.log(`from: ${from}, size: ${size}`);
+
+        return await this.service.searchMessages( {sort: ["_kuzzle_info.createdAt"], from, size} );
     }
 }
 
