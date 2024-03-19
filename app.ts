c618631efafdaf5723122d1db2df9ccbd39d90fe
InkedCat <146424693+InkedCat@users.noreply.github.com>
2024-03-20 00:06:17 +0100

    feat: register controller to the backend

--- app.ts
+++ app.ts
@@ -1,5 +1,14 @@
+import { ChatController } from "./lib/modules/ChatController";
 import { ChatTestApplication } from "./lib/ChatTestApplication";
+import { ChatService } from "./lib/modules/ChatService";
+
+const databaseName = "kuzzle-chatdb";
 
 const app = new ChatTestApplication(databaseName)
+
+const chatService = new ChatService(app);
+const chatController = new ChatController(app, chatService);
+
+app.controller.use(chatController);
 
 app.start();
