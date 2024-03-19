9bbeabfe7c43ac28c7f45eedb1e87232454aebc1
InkedCat <146424693+InkedCat@users.noreply.github.com>
2024-03-19 23:48:40 +0100

    feat: add chat service methods

---
+++ lib/modules/ChatService.ts
@@ -0,0 +1,25 @@
+import { JSONObject, KHit, SearchResult } from "kuzzle";
+import { ChatTestApplication } from "lib/ChatTestApplication";
+import { Message } from "lib/interfaces/Message";
+
+class ChatService {
+    private app: ChatTestApplication;
+
+    constructor(app: ChatTestApplication) {
+        this.app = app;
+    }
+
+    async createMessage(author: string, content: string) {
+        await this.app.sdk.document.create(this.app.getDatabaseName(), "chat-messages", {author, content});
+    }
+
+    async searchMessages(query: JSONObject): Promise<SearchResult<KHit<Message>>> {
+        return await this.app.sdk.document.search<Message>(this.app.getDatabaseName(), "chat-messages", query);
+    }
+
+    async notifyBadWord(author: string) {
+        await this.app.sdk.realtime.publish("kuzzle-chatdb", "chat-messages", {author, warning: "bad word"});
+    }
+}
+
+export { ChatService };
