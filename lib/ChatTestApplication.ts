ef3bdf6938d63ac1f3cdeda42ca751427bf4d1be
InkedCat <146424693+InkedCat@users.noreply.github.com>
2024-03-19 23:32:04 +0100

    feat: add getter for database name

--- lib/ChatTestApplication.ts
+++ lib/ChatTestApplication.ts
@@ -3,6 +3,10 @@ import { Backend } from "kuzzle";
 export class ChatTestApplication extends Backend {
   private databaseName: string;
 
+  getDatabaseName() {
+    return this.databaseName;
+  }
+
   constructor(databaseName: string) {
     super("chat-test-application");
     this.databaseName = databaseName;
